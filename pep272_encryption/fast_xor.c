0b3c9b535e5bed95886b4e992dda3b586b79a89e
Simon Biewald <simon@fam-biewald.de>
2020-09-22 00:48:08 +0200

    Define PY_SSIZE_T_CLEAN and don't mix Py_size_t and int

--- pep272_encryption/fast_xor.c
+++ pep272_encryption/fast_xor.c
@@ -1,7 +1,8 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
 #include <stdlib.h>
-
+#include <stdio.h>
 
 static PyObject* fast_xor(PyObject *self, PyObject *args, PyObject *kwargs) {
     Py_ssize_t a_length, b_length, length;
@@ -12,17 +13,19 @@ static PyObject* fast_xor(PyObject *self, PyObject *args, PyObject *kwargs) {
         return NULL;
     }
 
+    printf("%ld %ld\n", a_length, b_length);
+
     if (NULL == a || NULL == b) {
         PyErr_SetString(PyExc_TypeError, "One of the parameters is None");
         return NULL;
     }
 
-    if (0 == a_length || 0 == b_length) {
-        // Both are non-zero
-        return Py_BuildValue("y#", a, 0);
+    length = a_length < b_length ? a_length : b_length;
+
+    if (0 == length) {
+        return Py_BuildValue("y#", "\0", 0);
     }
 
-    length = a_length < b_length ? a_length : b_length;
     out = malloc(length);
 
     for (Py_ssize_t i = 0; i < length; i++) {
